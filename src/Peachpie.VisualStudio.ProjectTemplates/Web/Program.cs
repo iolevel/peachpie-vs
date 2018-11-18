f7c435531290441ee92b9bb64f8b80bedc50dd2b
Jakub Míšek <jmisek@gmail.com>
2018-11-18 15:19:38 +0100

    web project template

---
+++ src/Peachpie.VisualStudio.ProjectTemplates/Web/Program.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Peachpie.AspNetCore.Web;
+
+namespace $ext_safeprojectname$.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+	
+	class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseSession();
+
+            app.UsePhp(new PhpRequestOptions(scriptAssemblyName: "$ext_safeprojectname$"));
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
