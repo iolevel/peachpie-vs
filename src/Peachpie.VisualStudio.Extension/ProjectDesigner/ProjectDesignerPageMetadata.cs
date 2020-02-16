38e5a51443a411dc03c80a0164f17c66bc48b295
Jakub Míšek <jmisek@gmail.com>
2020-02-16 15:33:43 +0100

    Project Designer Pages - build, buildevents, pack, launch profiles, signing

---
+++ src/Peachpie.VisualStudio.Extension/ProjectDesigner/ProjectDesignerPageMetadata.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.ProjectSystem.VS.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peachpie.VisualStudio.Extension.ProjectDesigner
+{
+	/// <summary>
+	/// <see cref="IPageMetadata"/> implementation to be used for project property app designer.
+	/// </summary>
+	sealed class ProjectDesignerPageMetadata : IPageMetadata
+    {
+		public string Name { get; }
+
+		public bool HasConfigurationCondition { get; }
+
+		public Guid PageGuid { get; }
+
+		public int PageOrder { get; }
+
+		public ProjectDesignerPageMetadata(Guid pageGuid, int pageOrder, bool hasConfigurationCondition)
+		{
+			if (pageGuid == Guid.Empty)
+			{
+				throw new ArgumentException(null, nameof(pageGuid));
+			}
+
+			PageGuid = pageGuid;
+			PageOrder = pageOrder;
+			HasConfigurationCondition = hasConfigurationCondition;
+		}
+	}
+}
---
+++ src/Peachpie.VisualStudio.Extension/ProjectDesigner/ProjectDesignerPageProvider.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.VS.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peachpie.VisualStudio.Extension.ProjectDesigner
+{
+    [Export(typeof(IVsProjectDesignerPageProvider))]
+    [AppliesTo("PeachPie & AppDesigner")]
+    class ProjectDesignerPageProvider : IVsProjectDesignerPageProvider
+    {
+        readonly UnconfiguredProject _project;
+
+        IProjectCapabilitiesScope Capabilities => _project.Capabilities;
+
+        [ImportingConstructor]
+        internal ProjectDesignerPageProvider(UnconfiguredProject project)
+        {
+            _project = project;
+        }
+
+        public Task<IReadOnlyCollection<IPageMetadata>> GetPagesAsync()
+        {
+            var result = new List<IPageMetadata>(8);
+
+            // result.Add(Application); // only works for CSharp
+            result.Add(Build);
+            result.Add(BuildEvents);
+            if (Capabilities.Contains("Pack"))
+            {
+                result.Add(Package);
+            }
+            if (Capabilities.Contains("LaunchProfiles"))
+            {
+                result.Add(Debug);
+            }
+            result.Add(Signing);
+            //instance.Add(CodeAnalysis);
+            return Task.FromResult<IReadOnlyCollection<IPageMetadata>>(result);
+        }
+
+        // following property pages are taken from C# projects:
+
+        // static readonly ProjectDesignerPageMetadata Application = new ProjectDesignerPageMetadata(new Guid("{5E9A8AC2-4F34-4521-858F-4C248BA31532}"), 0, hasConfigurationCondition: false);
+
+        static readonly ProjectDesignerPageMetadata Build = new ProjectDesignerPageMetadata(new Guid("{A54AD834-9219-4aa6-B589-607AF21C3E26}"), 1, hasConfigurationCondition: true);
+
+        static readonly ProjectDesignerPageMetadata BuildEvents = new ProjectDesignerPageMetadata(new Guid("{1E78F8DB-6C07-4d61-A18F-7514010ABD56}"), 2, hasConfigurationCondition: false);
+
+        static readonly ProjectDesignerPageMetadata Package = new ProjectDesignerPageMetadata(new Guid("{21b78be8-3957-4caa-bf2f-e626107da58e}"), 3, hasConfigurationCondition: false);
+
+        static readonly ProjectDesignerPageMetadata Debug = new ProjectDesignerPageMetadata(new Guid("{0273C280-1882-4ED0-9308-52914672E3AA}"), 4, hasConfigurationCondition: false);
+
+        // static readonly ProjectDesignerPageMetadata ReferencePaths = new ProjectDesignerPageMetadata(new Guid("{031911C8-6148-4e25-B1B1-44BCA9A0C45C}"), 5, hasConfigurationCondition: false);
+
+        static readonly ProjectDesignerPageMetadata Signing = new ProjectDesignerPageMetadata(new Guid("{F8D6553F-F752-4DBF-ACB6-F291B744A792}"), 6, hasConfigurationCondition: false);
+
+        // static readonly ProjectDesignerPageMetadata CodeAnalysis = new ProjectDesignerPageMetadata(new Guid("{c02f393c-8a1e-480d-aa82-6a75d693559d}"), 7, hasConfigurationCondition: false);
+    }
+
+}
