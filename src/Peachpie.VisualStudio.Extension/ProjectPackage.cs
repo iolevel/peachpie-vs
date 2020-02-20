3fe276b475c7488182f6ddf20a007f2f63a66aa2
Jakub Míšek <jmisek@gmail.com>
2020-02-20 18:51:27 +0100

    0.9.810

--- src/Peachpie.VisualStudio.Extension/ProjectPackage.cs
+++ src/Peachpie.VisualStudio.Extension/ProjectPackage.cs
@@ -28,7 +28,7 @@ using Task = System.Threading.Tasks.Task;
 /// </summary>
 static class ProductVersion
 {
-    public const string VersionString = "0.9.800.1";    // matches source.extension.vsixmanifest
+    public const string VersionString = "0.9.810";    // matches source.extension.vsixmanifest
     public const string AssemblyVersion = VersionString;
     public const string FileVersion = VersionString;
 }
