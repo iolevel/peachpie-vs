f3c64694e0127dae3eb3b9a99ab208258e1e79d6
Jakub Míšek <jmisek@gmail.com>
2021-03-08 15:10:12 +0100

    peachpie v1.0.0; net5.0

--- src/Peachpie.VisualStudio.Extension/ProjectPackage.cs
+++ src/Peachpie.VisualStudio.Extension/ProjectPackage.cs
@@ -28,7 +28,7 @@ using Task = System.Threading.Tasks.Task;
 /// </summary>
 static class ProductVersion
 {
-    public const string VersionString = "1.0.5";    // matches source.extension.vsixmanifest
+    public const string VersionString = "1.0.6";    // matches source.extension.vsixmanifest
     public const string AssemblyVersion = VersionString;
     public const string FileVersion = VersionString;
 }
